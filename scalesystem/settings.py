8c1a810bcd471abb9f1055ecbaabbe62fca2fcc4
adolfo-univasf <adolfo_senai@hotmail.com>
2021-05-28 00:23:52 -0300

    teste_settings

--- scalesystem/settings.py
+++ scalesystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import  django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,6 +55,10 @@ cron: pip install django_cron
 matplotlib: pip install -U matplotlib
 
 """
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
