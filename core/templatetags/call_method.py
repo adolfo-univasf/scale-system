fcd13f43f044b03721e8fa6e227f79f714c800fd
jonataspassos <jonatascastropassos@gmail.com>
2021-06-01 18:06:08 -0300

    Restrict authorizated options

--- core/templatetags/call_method.py
+++ core/templatetags/call_method.py
@@ -1,4 +1,5 @@
 from django import template
+from ministries.utils import my_engaged,my_ministries
 
 register = template.Library()
 
@@ -7,4 +8,16 @@ def call_method(obj, method_name, *args):
     method = getattr(obj,method_name)
     return method(*args)
 
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
+@register.filter(name='has_ministry')
+def has_ministry(user):
+    if my_engaged(user) or my_ministries(user):
+        return True
+    return False
+    
+
 # {% call_method obj 'method_name' args %}
+# {% user|filtername : 'arg'%}
--- ministries/utils.py
+++ ministries/utils.py
@@ -1,16 +1,23 @@
 from accounts.models import User
 from .models import Ministry, Function
+from scales.models import ProgramTime
 from functools import reduce
 
 def my_engaged(user):
     def m(x):
         return [x.ministry]
     def r(x,a):
-        if x[0] not in a:
+        if x[0] and x[0] not in a:
             a.append(x[0])
         return a
-    functions = Function.objects.filter(people = user)
-    if functions.first():
+    functions = list(Function.objects.filter(people = user))
+
+    #funções que eu não estou na equipe mas fui colocado na escala
+    prog = ProgramTime.objects.filter(person = user)
+    for pg in prog:
+        functions.append(pg.function)
+
+    if functions:
         return reduce(r,map(m,functions))
     else:
         return []
