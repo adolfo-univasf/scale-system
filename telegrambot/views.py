c8911874807ba5e1f6be747a9e88e0554b2df047
jonataspassos <jonatascastropassos@gmail.com>
2021-06-08 09:23:01 -0300

    Fix error 'else'

--- telegrambot/views.py
+++ telegrambot/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from scales.utils import scale_string
 from programs.models import Program
+from django.utils.translation import gettext as _
 from datetime import datetime, date
 
 # Create your views here.
@@ -12,8 +13,13 @@ def program (request):
     if pg:
         pg = pg[0]
         response = {}
+        response['success'] = True
         response['name'] = str(pg)
         response['date'] = str(pg.date)
         response['program'] = scale_string(pg)
         return JsonResponse(response)  #, safe=False
     else:
+        response = {}
+        response['success'] = False
+        response['message'] = _("There is no next program")
+        return JsonResponse(response)  #, safe=False
